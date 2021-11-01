a280d33c9ae260aecdd4f300484a01b0821cf1fc
Your Name <you@example.com>
2021-11-01 03:05:59 +0000

    chage

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "job1" {
-    ami = "ami-07d02ee1eeb0c996c"
+    ami = "ami-089fe97bc00bff7cc"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.job1-sec-group.name]
     key_name = "macbook"
