9cbc94b1c202ee3b26c537ce879f1bf06da14d04
Your Name <you@example.com>
2021-11-05 23:22:55 +0000

    dasd

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "job1" {
     ami = "ami-089fe97bc00bff7cc"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.job1-sec-group.name]
-    key_name = "macbook"
+    key_name = aws_key_pair.job1-key.key_name
 
 
 
