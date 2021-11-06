4b2929be545bff49709d4d25936f1321728a7fa4
dave2121 <88213296+dave2121@users.noreply.github.com>
2021-11-05 19:10:55 -0500

    Update key-pair.tf

--- key-pair.tf
+++ key-pair.tf
@@ -1,3 +1,4 @@
 resource "aws_key_pair" "job1-key" {
   key_name   = "jenkins-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6IEdY0WaQl+jEE5Hb9FN0VfGpAZIPvqXMcJ71dfF+OJt9oinAoKPGXvIdgXRguZqRoUzyYlY9kWWd88oo1gnoUei/3pXB1tDpgRHOer3rbUCCmEgbJPYz43hjCu2kgmZJkpB7sWhHTQCC969RK8OIrLv+5NXv24v5O+OSwqSXxJvD7cMxHQDat3c966lfSV4kxdfnhle0moO130/3UfORDfycmxSge61EqzLdnBL42Be7GW3DltdJ29xZtCW5ErZy+OSsaoUTZMBZboXTDordHJdZQ/cv7D+21Lm991zi5/eCgR1zfXY9tTqbUtgSpL1j6wB1S/giaiqeWaRcpVVL admin@ip-172-31-15-72"
+}
